f6d983fe508ce662b3f57c6b0850bd9d53dbbab3
BogdanDomashenko <got.bogdan@gmail.com>
2022-03-09 01:00:53 +0200

    upd

---
+++ src/redux/slices/quizzesSlice.js
@@ -0,0 +1,11 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const quizzesSlice = createSlice({
+  name: "squares",
+  initialState: [],
+  reducers: {},
+});
+
+//export const {} = quizzesSlice.actions;
+
+export default squaresSlice.reducer;
